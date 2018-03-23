9b5d6781ddb47ba705968ef0e3981404bc0764f0
danse <f.occhipinti@gmail.com>
2018-03-23 11:27:17 +0100

    rewrite the test script

--- test.hs
+++ test.hs
@@ -1,25 +1,34 @@
 #!/usr/bin/env stack
--- stack --resolver lts-10.10 script --package turtle --package pandoc --package text --package foldl
+-- stack --resolver lts-10.10 script --package turtle --package pandoc --package text --package foldl --package system-filepath
 {-# LANGUAGE OverloadedStrings #-}
 import Turtle
 import Control.Applicative
-import Data.Text (pack, empty)
+import Data.Text (pack, unpack, empty)
 import Control.Foldl hiding (map)
 import Data.Maybe (fromJust)
+import Filesystem.Path (addExtension)
 
-toTestDir p = "tests" </> (filename $ dropExtension p)
 
 tempFile = "/tmp/pandoc-filter-automated-test.native"
 
-main = view testAll
+-- | get the filter to be used for tests in a dir
+--
+-- >>> filterFromDir (FilePath "tests/filter/3")
+-- "../../../filter.hs"
+filterFromDir p = filters <> name <> ".hs"
+  where name = pack $ init $ unpack $ format fp $ splitDirectories p !! 1
+        filters = "../../../filters/"
 
-testAll :: Shell Text
-testAll = do
-  filterName <- ls "filters"
-  testFilter filterName
+report :: ExitCode -> Turtle.FilePath -> Text
+report e = format (fp % " " % outcome e)
+  where outcome ExitSuccess = " success"
+        outcome (ExitFailure _) = " failure"
+
+pandoc d = format ("pandoc in.native -o "%s%" --filter="%s) tempFile f
+  where f = filterFromDir d
 
 echoText :: Text -> Shell ()
-echoText = echo . fromJust . textToLine
+echoText = if False then echo . fromJust . textToLine else const (pure ())
 
 showWorkingDir :: Shell ()
 showWorkingDir = do
@@ -36,27 +45,13 @@ inDir d s = do
   showWorkingDir
   pure res
 
-testFilter :: Turtle.FilePath -> Shell Text
-testFilter f = do
-  exists <- testpath (toTestDir f)
-  if exists
-    then inDir (toTestDir f) (testNumber f) 
-    else pure $ format ("no tests for "%fp) f
-
-testNumber :: Turtle.FilePath -> Shell Text
-testNumber f = do
-  number <- ls "."
-  inDir number (
+testAll = do
+  dir <- join $ ls <$> ls "tests"
+  inDir dir (
     do
-      --echo $ fromJust $ textToLine (pandoc f)
-      shell (pandoc f) Control.Applicative.empty
+      --echo $ fromJust $ textToLine (pandoc dir)
+      shell (pandoc dir) Control.Applicative.empty
       exit <- shell ("diff out.native " <> tempFile) Control.Applicative.empty
-      pure $ report exit f number)
+      pure $ report exit dir)
 
-pandoc f = format ("pandoc in.native -o "%s%" --filter=../../../"%fp) tempFile f
-
-report :: ExitCode -> Turtle.FilePath -> Turtle.FilePath -> Text
-report e = format (fp % " " % fp % outcome e)
-  where outcome ExitSuccess = " success"
-        outcome (ExitFailure _) = " failure"
-
+main = view testAll
