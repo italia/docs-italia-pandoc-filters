589652f6dd8a0b1926be386ca14f391ca1741571
danse <f.occhipinti@gmail.com>
2018-09-03 17:16:05 +0200

    semplifico il `filtro-stile-liste`

--- filters/filtro-stile-liste.hs
+++ filters/filtro-stile-liste.hs
@@ -3,15 +3,22 @@ import Text.Pandoc.JSON
 import Text.Pandoc.Definition
 import Data.List
 
-parafy (Plain i:bs) = Para i:bs
-parafy b = b
+{-
 
-onLists :: ([Block] -> [Block]) -> Block -> Block
-onLists f (BulletList i) = BulletList (map f i)
-onLists f (OrderedList a i) = OrderedList a (map f i)
-onLists f b = b
+"loose lists" are a conventional data model in pandoc, represented as
+lists ending with paragraphs. Compact lists are rendered with no empty
+lines in between, while items in a loose list are separated by empty
+lines
+
+-}
 
 loosen :: Block -> Block
-loosen = onLists parafy
+loosen (Plain bs) = Para bs
+loosen b = b -- pass other list items through
 
-main = toJSONFilter loosen
+f :: Block -> Block
+f (BulletList items) = BulletList (map (map loosen) items)
+f (OrderedList a items) = OrderedList a (map (map loosen) items)
+f b = b -- pass other blocks through
+
+main = toJSONFilter f
